5a6ac9eb8e9a505208aae1c074b405a58afc3403
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-11-03 18:31:58 +0700

    repackge new apk

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
+rm -rf extracted_apk || rm -rf repackaged.apk || rm -rf signed-apk ||
+mkdir extracted_apk &&
 apktool d origin.apk -o extracted_apk &&
 apktool b extracted_apk -o repackaged.apk &&
-java -jar uber-apk-signer-1.2.1.jar -a repackaged.apk --ks c:\<<path to keystore>>\key.jks --ksAlias key --ksKeyPass <<password>> --ksPass <<password>> -o app-release-mod-signed
+java -jar uber-apk-signer-1.2.1.jar -a repackaged.apk --ks ./key2/key_platform_2 --ksAlias key0 --ksKeyPass 234567 --ksPass 234567 -o signed-apk
 
